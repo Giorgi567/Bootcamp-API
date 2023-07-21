32c1174124737d706e15b13f777146edd3e871ff
Giorgi Gvintidze <giorgininidze200@gmail.com>
2023-07-21 22:10:54 +0400

    added secuiry mesurements noSql injection, xss protection for secure headers request limiting from same IP and Cors

--- src/app.js
+++ src/app.js
@@ -8,6 +8,14 @@ const path = require("path");
 const connectDB = require("./config/db.js");
 const cookieParser = require("cookie-parser");
 
+// ------------------Security Modules -------------------------//
+const mongoSanitze = require("express-mongo-sanitize");
+const helmet = require("helmet");
+const xss = require("xss-clean");
+const rateLimit = require("express-rate-limit");
+const hpp = require("hpp");
+const cors = require("cors");
+
 // ------------------Route Files -------------------------//
 const bootcampRoutes = require("./routes/bootcamps.routes.js");
 const courseRouter = require("./routes/courses.routes.js");
@@ -17,6 +25,9 @@ const reveiwsRoutes = require("./routes/reveiws.routes.js");
 
 // app
 const app = express();
+
+// Makes API able to communicate with Different domain
+app.use(cors());
 
 // Parses data comming from body
 app.use(express.json());
@@ -37,6 +48,25 @@ app.use(express.static(path.join(__dirname, "public")));
 
 // DataBase Connect
 connectDB();
+
+// Sanitize Data
+app.use(mongoSanitze());
+
+// Set Security Headers
+app.use(helmet());
+
+// Prevents Cross-Site Scripts in our database
+app.use(xss());
+
+// Request Rate limiting
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 1000, // 10 min
+  max: 100, //amout of request user can make with same IP withint 10 min(windowMs)
+});
+app.use(limiter);
+
+//Prevents http param polution
+app.use(hpp());
 
 // Mount Routers
 app.use("/api/v1/bootcamps", bootcampRoutes);
